551877359a738e4d188f5331185651d5882d4abb
Ashton Chen <cdljsj@gmail.com>
2023-11-15 22:20:11 +0800

    feat: update coin name to WEN

--- packages/contracts/contracts/LUSDToken.sol
+++ packages/contracts/contracts/LUSDToken.sol
@@ -28,8 +28,8 @@ contract LUSDToken is CheckContract, ILUSDToken {
     using SafeMath for uint256;
     
     uint256 private _totalSupply;
-    string constant internal _NAME = "ioNOTE Stablecoin";
-    string constant internal _SYMBOL = "ioNOTE";
+    string constant internal _NAME = "WEN";
+    string constant internal _SYMBOL = "WEN";
     string constant internal _VERSION = "1";
     uint8 constant internal _DECIMALS = 18;
     
